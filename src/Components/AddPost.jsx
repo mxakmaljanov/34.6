69f0dcd2562a06f5fc1b60c4237f6ad131bfc9c4
mxakmaljanov <maxmudxonakmaljanov@gmail.com>
2025-02-16 21:59:38 +0500

    34.6

--- src/Components/AddPost.jsx
+++ src/Components/AddPost.jsx
@@ -1,21 +1,40 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from "../services/axiosInstance.js";
 import '../index.css';
 
 function AddPost() {
     const [title, setTitle] = useState("");
     const [body, setBody] = useState("");
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!title || !body) {
+            alert("hammasini to'ldirmadizu Bos!");
+            return;
+        }
+
+        axios.post("/posts", { title, body })
+            .then(() => {
+                alert("Postingiz muvaffaqiyatli qoshildi Bos!");
+                navigate("/");
+            })
+            .catch(() => alert("qandaydir hatolik bor shekilli Bos!"));
+    };
+
     return (
         <div className="container2">
             <h2>Add Post</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input
                     type="text"
-                    placeholder="Post nomini kiriting...!"
+                    placeholder="Post nomini kiriting iltimos...!"
                     value={title}
                     onChange={(e) => setTitle(e.target.value)}
                 />
                 <textarea
-                    placeholder="Post matnini kiriting...!"
+                    placeholder="Post matnini kiriting iltimos...!"
                     value={body}
                     onChange={(e) => setBody(e.target.value)}
                 ></textarea>
